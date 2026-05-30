716709151d4df82e3a55d84bfa975a520fb9cdfa
Roman Mazur <roman@rmazur.io>
2026-05-30 22:15:31 +0200

    edit/internal/content: extract SaveToWriter

--- internal/content/save.go
+++ internal/content/save.go
@@ -3,6 +3,7 @@ package content
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"time"
@@ -18,15 +19,8 @@ func Save(content Interface, dst string) error {
 	defer f.Close()
 
 	w := bufio.NewWriter(f)
-	for i, line := range content.Lines() {
-		if i > 0 {
-			if _, err := w.WriteString("\n"); err != nil {
-				return err
-			}
-		}
-		if _, err := w.WriteString(line.String()); err != nil {
-			return err
-		}
+	if err := SaveToWriter(content, w); err != nil {
+		return err
 	}
 	if err := w.Flush(); err != nil {
 		return err
@@ -36,6 +30,20 @@ func Save(content Interface, dst string) error {
 	}
 
 	return os.Rename(tmpPath, dst)
+}
+
+func SaveToWriter(content Interface, out io.Writer) error {
+	for i, line := range content.Lines() {
+		if i > 0 {
+			if _, err := out.Write([]byte("\n")); err != nil {
+				return err
+			}
+		}
+		if _, err := out.Write([]byte(line.String())); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func tmpDir() string {
